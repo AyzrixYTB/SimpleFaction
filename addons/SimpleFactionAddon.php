be31f0ec33e918046088d3ec6134e4a169db8b90
Ayzrix <32325042+AyzrixYTB@users.noreply.github.com>
2021-03-17 04:19:58 +0100

    Added ScoreHUD custom addon

---
+++ addons/SimpleFactionAddon.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @name SimpleFactionAddon
+ * @version 1.0.0
+ * @main JackMD\ScoreHud\Addons\SimpleFactionAddon
+ * @depend SimpleFaction
+ */
+namespace JackMD\ScoreHud\Addons
+{
+
+    use Ayzrix\SimpleFaction\API\FactionsAPI;
+    use JackMD\ScoreHud\addon\AddonBase;
+	use pocketmine\Player;
+
+	class SimpleFactionAddon extends AddonBase {
+
+		/**
+		 * @param Player $player
+		 * @return array
+		 */
+		public function getProcessedTags(Player $player): array{
+			return [
+				"{faction_name}" => $this->getPlayerFaction($player),
+				"{faction_power}" => $this->getFactionPower($player),
+                "{faction_rank}" => $this->getFactionRank($player)
+			];
+		}
+
+		/**
+		 * @param Player $player
+		 * @return string
+		 */
+		public function getPlayerFaction(Player $player): string{
+            if (FactionsAPI::isInFaction($player)) {
+                return FactionsAPI::getFaction($player);
+            } else return "No Faction";
+		}
+
+        /**
+         * @param Player $player
+         * @return string|int
+         */
+        public function getFactionPower(Player $player) {
+            if (FactionsAPI::isInFaction($player)) {
+                return FactionsAPI::getPower(FactionsAPI::getFaction($player));
+            } else return "No Faction";
+        }
+
+        /**
+         * @param Player $player
+         * @return string
+         */
+        public function getFactionRank(Player $player): string {
+		    if (FactionsAPI::isInFaction($player)) {
+		        return FactionsAPI::getRank($player->getName());
+            } else return "No Faction";
+        }
+	}
+}
