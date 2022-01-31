be6f44f76db8cffa9f08ccfcdca6e7e7c059c83a
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-01-30 20:12:46 -0500

    API4

--- src/Ayzrix/SimpleFaction/Tasks/BorderTask.php
+++ src/Ayzrix/SimpleFaction/Tasks/BorderTask.php
@@ -15,30 +15,28 @@ namespace Ayzrix\SimpleFaction\Tasks;
 
 use Ayzrix\SimpleFaction\API\FactionsAPI;
 use Ayzrix\SimpleFaction\Utils\Utils;
-use pocketmine\level\particle\DustParticle;
+use pocketmine\color\Color;
 use pocketmine\math\Vector3;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
+use pocketmine\world\format\Chunk;
+use pocketmine\world\particle\DustParticle;
 
 class BorderTask extends Task {
 
-    /**
-     * @param int $currentTick
-     * @return bool
-     */
-    public function onRun(int $currentTick): bool {
-        if (empty(FactionsAPI::$border)) return false;
+    public function onRun(): void {
+        if (empty(FactionsAPI::$border)) return;
         foreach (FactionsAPI::$border as $name => $bool) {
-            $player = Server::getInstance()->getPlayer($name);
+            $player = Server::getInstance()->getPlayerExact($name);
             if ($player instanceof Player) {
-                $level = $player->getLevel();
-                $chunk = $level->getChunkAtPosition($player);
-                $chunkX = $chunk->getX();
-                $chunkZ = $chunk->getZ();
-                $minX = (float)$chunk->getX() * 16;
+                $level = $player->getWorld();
+                $pos = $player->getPosition()->asVector3();
+                $chunkX = $pos->getFloorX() >> Chunk::COORD_BIT_SIZE;
+                $chunkZ = $pos->getFloorZ() >> Chunk::COORD_BIT_SIZE;
+                $minX = (float)$chunkX * 16;
                 $maxX = (float)$minX + 16;
-                $minZ = (float)$chunk->getZ() * 16;
+                $minZ = (float)$chunkZ * 16;
                 $maxZ = (float)$minZ + 16;
 
                 for ($x = $minX; $x <= $maxX; $x += 0.5) {
@@ -48,20 +46,19 @@ class BorderTask extends Task {
                             if ($claimedBy !== "") {
                                 if (FactionsAPI::isInFaction($player->getName())) {
                                     $color = Utils::getBorderColor(true, $claimedBy, FactionsAPI::getFaction($player->getName()));
-                                    $player->getLevel()->addParticle(new DustParticle(new Vector3($x, $player->getY() + 0.8, $z), (int)$color[0], (int)$color[1], (int)$color[2]), [$player]);
+                                    $player->getWorld()->addParticle(new Vector3($x, $pos->getY() + 0.8, $z),new DustParticle(new Color((int)$color[0], (int)$color[1], (int)$color[2])),[$player]);
                                 } else {
                                     $color = Utils::getBorderColor(true, $claimedBy);
-                                    $player->getLevel()->addParticle(new DustParticle(new Vector3($x, $player->getY() + 0.8, $z), (int)$color[0], (int)$color[1], (int)$color[2]), [$player]);
+                                    $player->getWorld()->addParticle(new Vector3($x, $pos->getY() + 0.8, $z),new DustParticle(new Color((int)$color[0], (int)$color[1], (int)$color[2])), [$player]);
                                 }
                             } else {
                                 $color = Utils::getBorderColor(false);
-                                $player->getLevel()->addParticle(new DustParticle(new Vector3($x, $player->getY() + 0.8, $z), (int)$color[0], (int)$color[1], (int)$color[2]), [$player]);
+                                $player->getWorld()->addParticle(new Vector3($x, $pos->getY() + 0.8, $z),new DustParticle(new Color((int)$color[0], (int)$color[1], (int)$color[2])), [$player]);
                             }
                         }
                     }
                 }
             } else unset(FactionsAPI::$border[$name]);
         }
-        return true;
     }
 }
